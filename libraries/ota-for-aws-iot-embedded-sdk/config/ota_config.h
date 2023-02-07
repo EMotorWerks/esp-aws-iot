48ff8b486b5c9e9befab04acad95c1967d657d5c
Nollupp <nollupp@gmail.com>
2023-02-07 15:19:33 -0600

    Changed OTA self test response time to 15

--- libraries/ota-for-aws-iot-embedded-sdk/config/ota_config.h
+++ libraries/ota-for-aws-iot-embedded-sdk/config/ota_config.h
@@ -104,7 +104,7 @@
 /**
  * @brief Milliseconds to wait for the self test phase to succeed before we force reset.
  */
-#define otaconfigSELF_TEST_RESPONSE_WAIT_MS     600000U
+#define otaconfigSELF_TEST_RESPONSE_WAIT_MS     900000U
     
 /**
  * @brief Milliseconds to wait before requesting data blocks from the OTA service if nothing is happening.
